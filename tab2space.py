35b12cf073aa07bf72a6c90170009d5db27b0278
runningwolf <runningwolf2016@163.com>
2015-08-20 23:27:32 +0800

    Update tab2space.py

--- tab2space.py
+++ tab2space.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
-请在Python3下运行此程序='Please run this program with Python3'
-'''table to space'''
+
 import sys
 
 TAB_SIZE = 4
 
-allcontent, content_tab2space = [], []
 filenames = sys.argv[1:]
 for filename in filenames:
     with open(filename, 'r', encoding='utf-8') as f:
         # print(help(f))
         allcontent = f.readlines()
+    
+    allcontent2 = []
+    for item in allcontent:
+        allcontent2.append(item.replace('\t', ' '*TAB_SIZE))
 
-for item in allcontent:
-    content_tab2space.append(item.replace('\t', ' '*TAB_SIZE))
-
-with open('tab2space_'+filename, 'w', encoding='utf-8') as f:
-    f.writelines(content_tab2space)
+    with open('new_'+filename, 'w', encoding='utf-8') as f:
+        f.writelines(allcontent2)
